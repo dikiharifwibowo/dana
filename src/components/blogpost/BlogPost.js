d8cc5f4d224b06d5829db2a2153f1084f9ba09ac
dikih arif wibowo <dikih@qiscus.work>
2021-12-16 22:27:13 +0700

    improve home : toggle love post

--- src/components/blogpost/BlogPost.js
+++ src/components/blogpost/BlogPost.js
@@ -4,13 +4,26 @@ let PageSize = 16;
 
 const BlogPost = (props) => {
     const [currentPage, setCurrentPage] = useState(1);
-    let dataPageFirst = props.postRecords.slice(0, 16)
-    const currentTableData = useMemo(() => {
+    const [currentTableData, setCurrentTableData] = useState([]);
+    const [dataPageFirst, setdataPageFirst] = useState([]);
+    const data = useMemo(() => {
         const firstPageIndex = (currentPage - 1) * PageSize;
         const lastPageIndex = firstPageIndex + PageSize;
         return props.postRecords.slice(firstPageIndex, lastPageIndex);
     }, [currentPage]);
     
+    let handleLovePost = (index, page) => {
+        //currentTableData[index].id = "test"
+        if(page==='firstPage') {
+            setdataPageFirst([...dataPageFirst, dataPageFirst[index].active = !dataPageFirst[index].active ])
+        } else {
+            setCurrentTableData([...currentTableData, currentTableData[index].active = !currentTableData[index].active ])
+        }
+    };
+    useEffect(() => {
+        setdataPageFirst(props.postRecords.slice(0, 16))
+        setCurrentTableData(data)
+    });
     return (
         <React.Fragment>
             <div class="container section blog">
@@ -21,11 +34,11 @@ const BlogPost = (props) => {
                             <div class="column toaster is-3 ">
                                 <div  key={index} class="img-card" style={{backgroundImage: `url(${props.imageRandom})` }}>
                                     <div class="share">
-                                        <ion-icon name="heart-outline"></ion-icon>
+                                        <ion-icon class={ item.active ? `md hydrated red` : `md hydrated`} name={ item.active ? `heart` : `heart-outline`} onClick={() => handleLovePost(index)}></ion-icon>
                                         <ion-icon name="share-social-outline"></ion-icon>
                                     </div>
                                     <div class="content">
-                                        <p class="title is-5">Lorem ipsum</p>
+                                        <p class="title is-5">static title to {item.id}</p>
                                         <p class="subtitle is-6">{ props.staticRandom.substring(0, 40, )+`...` }</p>
                                     </div>
                                 </div>
@@ -38,12 +51,12 @@ const BlogPost = (props) => {
                             <div class="column toaster is-3 ">
                                 <div  key={index} class="img-card" style={{backgroundImage: `url(${props.imageRandom})` }}>
                                     <div class="share">
-                                        <ion-icon name="heart-outline"></ion-icon>
+                                        <ion-icon class={ item.active ? `md hydrated red` : `md hydrated`} name={ item.active ? `heart` : `heart-outline`} onClick={() => handleLovePost(index, 'firstPage')}></ion-icon>
                                         <ion-icon name="share-social-outline"></ion-icon>
                                     </div>
                                     <div class="content">
-                                        <p class="title is-5">Lorem ipsum</p>
-                                        <p class="subtitle is-6">{ props.staticRandom.substring(0, 40)+`...` }</p>
+                                        <p class="title is-5">static title to {item.id}</p>
+                                        <p class="subtitle is-6">{ props.staticRandom.substring(0, 40, )+`...` }</p>
                                     </div>
                                 </div>
                             </div>
